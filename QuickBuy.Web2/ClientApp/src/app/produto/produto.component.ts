1b0174b4c643274beca9c866b723f4d3f04617c1
Felipe Ferrari <fferrari@concentsistemas.com.br>
2020-03-05 16:30:50 -0300

    Aula 72

---
+++ QuickBuy.Web2/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-produto",
+  template: "<html><body>{{ obterNome() }}</body></html>",
+  
+})
+
+
+export class ProdutoComponent {
+  public nome: string = "Samsung";
+  public liberadoParaVenda: boolean;
+
+  public obterNome(): string {
+    return this.nome;
+  }
+
+}
