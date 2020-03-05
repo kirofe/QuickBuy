51271320a2d078b79943289959e94c8a65b907af
Felipe Ferrari <fferrari@concentsistemas.com.br>
2020-03-05 10:17:54 -0300

    Aula 54

---
+++ QuickBuy.Web2/Controllers/ProdutoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProdutoController : Controller
+    {
+
+        private readonly IProdutoRepositorio produtoRepositorio;
+        public ProdutoController(IProdutoRepositorio produtoRepositorio)
+        {
+            this.produtoRepositorio = produtoRepositorio; 
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            try
+            {
+                return Ok(this.produtoRepositorio.ObterTodos());                
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto) 
+        {
+            try
+            {
+                this.produtoRepositorio.Adicionar(produto);
+                return Created("api/produto", produto);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.ToString());
+            }
+        }
+    }
+}
--- QuickBuy.Web2/Startup.cs
+++ QuickBuy.Web2/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuickBuy.Dominio.Contratos;
 using QuickBuy.Repositorio.Contexto;
+using QuickBuy.Repositorio.Repositorios;
 
 namespace QuickBuy.Web2
 {
@@ -30,6 +32,8 @@ namespace QuickBuy.Web2
             var connectionString = Configuration.GetConnectionString("QuickBuyDB");
             services.AddDbContext<QuickBuyContexto>(option => option.UseLazyLoadingProxies()
                                                                     .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+
+            services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
