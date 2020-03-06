d616075d99f01be93049a409189b2f6cca2b8b65
Felipe Ferrari <fferrari@concentsistemas.com.br>
2020-03-06 17:23:05 -0300

    Aula 89

---
+++ QuickBuy.Web2/ClientApp/src/app/servicos/usuario/usuario.service.ts
@@ -0,0 +1,11 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor() { }
+}
