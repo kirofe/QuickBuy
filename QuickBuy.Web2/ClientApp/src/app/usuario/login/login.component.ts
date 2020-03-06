a2e63d0be42f93a63b95d9d703141def143137a3
Felipe Ferrari <fferrari@concentsistemas.com.br>
2020-03-06 16:25:00 -0300

    Aula 95

--- QuickBuy.Web2/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web2/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../modelo/usuario';
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -12,19 +12,25 @@ export class LoginComponent implements OnInit {
   public pikachu = "../../assets/detectivepikachu.jpg";
   public titulo  = "Pikachu";  
   public usuario;
+  public returnUlr: string;
 
-  constructor(private router: Router) {
-    this.usuario = new Usuario();
+  constructor(
+    private router: Router,
+    private activatedRouter: ActivatedRoute
+  ) {
+     
   }
 
   entrar() {
     if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "123") {
       sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUlr]);
     }
   }
 
   ngOnInit() {
+    this.returnUlr = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    this.usuario   = new Usuario();   
   }
 
 }
