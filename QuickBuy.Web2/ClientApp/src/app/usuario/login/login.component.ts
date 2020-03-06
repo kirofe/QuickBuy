6798cb6e8a12d9277ee37e1a75bc25aa3046f287
Felipe Ferrari <fferrari@concentsistemas.com.br>
2020-03-06 08:34:29 -0300

    Aula 80

--- QuickBuy.Web2/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web2/ClientApp/src/app/usuario/login/login.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LoginComponent implements OnInit {
-  public pikachu = "../assets/detectivepikachu.jpg";
+  public pikachu = "../../assets/detectivepikachu.jpg";
   public titulo = "Pikachu";
 
   constructor() { }
+
+  on_keypress() {
+    alert("clickei xD");
+  }
+
+  entrar() {
+    alert("Hello World!!!!!");
+  }
 
   ngOnInit() {
   }
